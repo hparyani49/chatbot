f00d31fe9e91d52f2f30253ca33b0d334a8ef9c4
Hitesh Paryani <hitesh.paryani@justdial.com>
2020-07-20 01:44:53 +0530

    Updates

---
+++ precache-manifest.0e030b2ffd092f38928270821e74427c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727ece7295f2e712dc0ea2ee27471d7d",
+    "url": "/chat-bot/index.html"
+  },
+  {
+    "revision": "e31119886b9427aa652c",
+    "url": "/chat-bot/static/css/main.f47b970b.chunk.css"
+  },
+  {
+    "revision": "b3075e9ecfa986eeb43c",
+    "url": "/chat-bot/static/js/2.797e2cc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-bot/static/js/2.797e2cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31119886b9427aa652c",
+    "url": "/chat-bot/static/js/main.04ae25bc.chunk.js"
+  },
+  {
+    "revision": "4ecb44ca68413f060401",
+    "url": "/chat-bot/static/js/runtime-main.5b3c1a9d.js"
+  }
+]);
